c40d10c609166e13ca920b6bae4b94dfba97950c
Jeferson Oliveira <jeferson.contas@gmail.com>
2021-05-23 21:00:54 -0300

    Exerc. 2 e 3

--- fundamentos/bloco_4/dia_3/exerc2.js
+++ fundamentos/bloco_4/dia_3/exerc2.js
@@ -3,8 +3,8 @@ let n = 5;
 let ast = '*';
 let linha = '';
 
-for (let index = 0; index < n; index += 1) {
+for (let index = 0; index <= n; index += 1) {
+  console.log(linha);
   linha = linha + ast;
-console.log(linha);
 }
 
